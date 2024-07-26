951743081651a45a531c7ffdf729b5af3b9a67e9
Ricardo Coelho <ricardo.coelho@abacus.ch>
2024-07-26 10:55:07 +0200

    feat(AppBar): create component and add MenuLanguage

---
+++ src/components/MenuLanguage.vue
@@ -0,0 +1,68 @@
+<template>
+  <v-menu location="bottom" color="primary">
+    <template #activator="{ props: ActivatorProps }">
+      <v-btn
+        v-bind="ActivatorProps"
+        :icon="$vuetify.display.xs ? 'mdi-logout' : undefined"
+        :size="$vuetify.display.xs ? 'small' : undefined"
+      >
+        <span v-if="hideFullText" class="text-uppercase">
+          {{ model ? model.slice(0, 2) : '' }}
+        </span>
+        <span v-else>
+          {{ getLanguageTextByIsoCode(model) }}
+        </span>
+      </v-btn>
+    </template>
+
+    <v-list>
+      <v-list-item
+        v-for="item in availableLocales"
+        :key="item"
+        link
+        color="primary"
+        @click="model = item"
+      >
+        <v-list-item-title>
+          {{ getLanguageTextByIsoCode(item) }}
+        </v-list-item-title>
+      </v-list-item>
+    </v-list>
+  </v-menu>
+</template>
+
+<script lang="ts" setup>
+import { type PropType } from 'vue';
+
+defineProps({
+  availableLocales: {
+    type: Array as PropType<string[]>,
+    default: () => []
+  },
+  hideIcon: {
+    type: Boolean,
+    default: false
+  },
+  hideFullText: {
+    type: Boolean,
+    default: false
+  }
+});
+
+const model = defineModel({ type: String, required: true });
+
+function getLanguageTextByIsoCode(isoCode: string) {
+  switch (isoCode) {
+    case 'de':
+      return 'Deutsch';
+    case 'en':
+      return 'English';
+    case 'fr':
+      return 'Français';
+    case 'it':
+      return 'Italiano';
+    default:
+      return '';
+  }
+}
+</script>
---
+++ src/components/TheAppBar.vue
@@ -0,0 +1,63 @@
+<template>
+  <v-app-bar flat>
+    <v-img src="/logos/abacus.svg" max-width="200" width="150"> </v-img>
+    <v-spacer></v-spacer>
+    <template #append>
+      <div
+        class="d-flex"
+        :class="{
+          'ga-1': $vuetify.display.xs,
+          'ga-5': $vuetify.display.smAndUp
+        }"
+      >
+        <MenuLanguage
+          v-model="locale"
+          :available-locales="availableLocalesFiltered"
+          hide-full-text
+        />
+        <VBtnPrimary
+          v-if="!currentUser"
+          href="https://summer-party.events.abacus.ch/"
+          :icon="$vuetify.display.xs ? 'mdi-clipboard-text' : undefined"
+          :size="$vuetify.display.xs ? 'small' : undefined"
+          :text="$vuetify.display.smAndUp ? $t('labls.programm') : undefined"
+        >
+        </VBtnPrimary>
+        <VBtnPrimary
+          v-if="currentUser"
+          :icon="$vuetify.display.xs ? 'mdi-logout' : undefined"
+          :size="$vuetify.display.xs ? 'small' : undefined"
+          :text="$vuetify.display.smAndUp ? $t('labls.logout') : undefined"
+          @click="logout"
+        >
+        </VBtnPrimary>
+        <VBtnPrimary
+          v-else
+          :icon="$vuetify.display.xs ? 'mdi-login' : undefined"
+          :to="{ name: 'auth-sign-in' }"
+          :size="$vuetify.display.xs ? 'small' : undefined"
+          :text="$vuetify.display.smAndUp ? $t('labls.login') : undefined"
+        >
+        </VBtnPrimary>
+      </div>
+    </template>
+  </v-app-bar>
+</template>
+
+<script setup lang="ts">
+import useAuthUser from '@/composables/auth-user';
+import { useAuthStore } from '@/stores/auth';
+import MenuLanguage from './MenuLanguage.vue';
+import { useI18n } from 'vue-i18n';
+
+const { currentUser } = useAuthStore();
+const { logout } = useAuthUser();
+
+const { locale, availableLocales } = useI18n();
+
+const availableLocalesFiltered = computed(() =>
+  availableLocales.filter(
+    (availableLocale: string) => availableLocale !== locale.value
+  )
+);
+</script>

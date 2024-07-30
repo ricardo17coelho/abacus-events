401d2e1188de396fb1ae0876103a1e5f91899a9d
Ricardo Coelho <ricardo.coelho@abacus.ch>
2024-07-30 16:09:43 +0200

    feat(SchuttleSchedule): add initial version

--- src/views/ShuttleSchedule.vue
+++ src/views/ShuttleSchedule.vue
@@ -5,10 +5,41 @@
         <AppTitle :title="$t('title.shuttle_schedule')" />
       </v-col>
     </v-row>
-    #TODO
+    <ContainerCentered class="px-0">
+      <v-card flat max-width="500" class="px-2">
+        <v-alert
+          class="my-6"
+          type="warning"
+          variant="tonal"
+          border="start"
+          rounded="lg"
+        >
+          PAUSE 19.00-21.30
+        </v-alert>
+        <v-card class="my-6" title="Hinfahrt">
+          <template #subtitle> 13:30-19:00 Uhr (im 30min Takt) </template>
+          <v-card-text>
+            St.Gallen Bahnhof, Lagerstrasse Via Haltestelle Hodlerstrasse nach
+            Abacus, Bruggwaldstrasse Shuttlefahrten
+          </v-card-text>
+        </v-card>
+
+        <v-card class="my-6" title="Rücktransport">
+          <template #subtitle> 21:30-01:30 Uhr (im 30min Takt) </template>
+          <v-card-text>
+            <div>
+              Abacus, Bruggwaldstrasse via Hodlerstrasse/Hotel Radisson (Falls
+              Fahrgäste dort aussteigen wollen-HALT AUF VERLANGEN) nach
+              St.Gallen Bahnhof, Lagerstrasse Danach: Shuttlefahrten
+            </div>
+          </v-card-text>
+        </v-card>
+      </v-card>
+    </ContainerCentered>
   </v-container>
 </template>
 
 <script setup lang="ts">
 import AppTitle from '@/components/app/AppTitle.vue';
+import ContainerCentered from '@/components/containers/ContainerCentered.vue';
 </script>

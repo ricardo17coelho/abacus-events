505f18e666c541bd4df04e51c92dec42d967f3d3
Ricardo Coelho <ricardo.coelho@abacus.ch>
2024-08-15 14:09:08 +0200

    fix(HomeView): render dynamic cards

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -15,13 +15,18 @@
   </v-container>
   <v-container class="align-center">
     <v-row>
-      <v-col v-for="link in links" :key="link.id" cols="12" sm="6" lg="3">
+      <v-col v-for="link in sortedLinks" :key="link.id" cols="12" sm="6" lg="3">
         <v-card
           variant="tonal"
           color="primary"
           :to="link.to"
           height="100%"
           :density="$vuetify.display.xs ? 'compact' : 'default'"
+          @click="
+            link.action && Object.keys(link).includes('action')
+              ? link.action()
+              : undefined
+          "
         >
           <template #title>
             <v-card-title class="text-h6 text-sm-h5">
@@ -41,26 +46,6 @@
           </template>
         </v-card>
       </v-col>
-      <v-col>
-        <v-card
-          variant="tonal"
-          color="primary"
-          height="100%"
-          :density="$vuetify.display.xs ? 'compact' : 'default'"
-          @click="showGallery = true"
-        >
-          <template #title>
-            <v-card-title class="text-h6 text-sm-h5">
-              {{ $t('labels.plan') }}
-            </v-card-title>
-          </template>
-          <template #append>
-            <v-avatar rounded="0" size="40">
-              <v-icon size="40" color="primary">mdi-map</v-icon>
-            </v-avatar>
-          </template>
-        </v-card>
-      </v-col>
     </v-row>
     <AppImagesView v-model="showGallery" :images="['/images/plan.png']" />
   </v-container>
@@ -70,15 +55,26 @@
 import { useI18n } from 'vue-i18n';
 import AppLiveLabel from '../components/app/AppLiveLabel.vue';
 import AppImagesView from '@/components/app/AppImagesView.vue';
+import { useAuthStore } from '@/stores/auth';
 
 const { t } = useI18n();
 
+const { isCurrentUserAdminOrHelper } = useAuthStore();
+
 const links = computed(() => [
+  {
+    id: 'dashboard',
+    title: 'Dashboard',
+    icon: 'mdi-view-dashboard',
+    show: () => isCurrentUserAdminOrHelper,
+    to: { name: 'dashboard' }
+  },
   {
     id: 'program',
     title: t('home.links.program.title'),
     description: t('home.links.program.description'),
     icon: 'mdi-clipboard-text',
+    show: () => true,
     to: { name: 'program' }
   },
   {
@@ -86,6 +82,7 @@ const links = computed(() => [
     title: t('home.links.parking.title'),
     description: t('home.links.parking.description'),
     icon: 'mdi-car',
+    show: () => true,
     to: { name: 'parking' }
   },
   {
@@ -93,9 +90,22 @@ const links = computed(() => [
     title: t('home.links.shuttle_schedule.title'),
     description: t('home.links.shuttle_schedule.description'),
     icon: 'mdi-bus',
+    show: () => true,
     to: { name: 'shuttle-schedule' }
+  },
+  {
+    id: 'plan',
+    title: t('labels.plan'),
+    description: undefined,
+    icon: 'mdi-map',
+    show: () => true,
+    action: () => {
+      showGallery.value = true;
+    }
   }
 ]);
+
+const sortedLinks = computed(() => links.value.filter((l) => l.show()));
 
 const showGallery = ref(false);
 </script>

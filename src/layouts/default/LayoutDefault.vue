f098f82d981f3b05e62fbe239730ae027bd132d3
Ricardo Coelho <ricardo.coelho@abacus.ch>
2025-04-29 21:10:45 +0200

    feat(LayoutDefault): show Footer

--- src/layouts/default/LayoutDefault.vue
+++ src/layouts/default/LayoutDefault.vue
@@ -1,16 +1,16 @@
 <template>
-  <v-layout>
-    <TheAppBar />
+  <TheAppBar />
+  <v-main class="overflow-auto">
+    <TheLayoutContainer>
+      <RouterView />
+    </TheLayoutContainer>
+  </v-main>
 
-    <v-main class="overflow-auto">
-      <TheLayoutContainer>
-        <RouterView />
-      </TheLayoutContainer>
-    </v-main>
-  </v-layout>
+  <TheFooter />
 </template>
 
 <script lang="ts" setup>
 import TheAppBar from '@/layouts/components/TheAppBar.vue';
 import TheLayoutContainer from '@/layouts/components/TheLayoutContainer.vue';
+import TheFooter from '@/layouts/components/TheFooter.vue';
 </script>

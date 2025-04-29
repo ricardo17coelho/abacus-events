8c552161a84eebba36547e987869c864fd7ec205
Ricardo Coelho <ricardo.coelho@abacus.ch>
2025-04-29 22:55:48 +0200

    WIP

--- src/components/UserGreetings.vue
+++ src/components/UserGreetings.vue
@@ -2,7 +2,7 @@
   <header>
     <h1 v-if="user" class="Title">
       <span>
-        {{ i18n.t(greetingMsg) }}
+        {{ greetingMsg }}
       </span>
       <span v-if="userDisplayName" class="mx-2 text-grey">
         {{ userDisplayName }}
@@ -30,7 +30,7 @@ import { useI18n } from 'vue-i18n';
 
 const { user, userDisplayName, userRoles } = useAuthUser();
 
-const i18n = useI18n();
+const { t } = useI18n();
 const { randomDirection } = useConfetti();
 
 const currentTime = ref();
@@ -52,14 +52,11 @@ onUnmounted(() => {
 
 const greetingMsg = computed(() => {
   const hrs = currentTime.value?.getHours();
-  let greet = '';
 
-  if (hrs >= 4 && hrs < 12) greet += 'good-morning';
-  else if (hrs >= 11 && hrs < 17) greet += 'good-afternoon';
-  else if (hrs >= 17 && hrs < 23) greet += 'good-evening';
-  else greet += 'hello';
-
-  return greet;
+  if (hrs >= 4 && hrs < 12) return t('greeting.good-morning');
+  else if (hrs >= 11 && hrs < 17) return t('greeting.good-afternoon');
+  else if (hrs >= 17 && hrs < 23) return t('greeting.good-evening');
+  else return t('greeting.hello');
 });
 </script>
 
@@ -68,32 +65,3 @@ const greetingMsg = computed(() => {
   z-index: 900;
 }
 </style>
-
-<i18n lang="json">
-{
-  "de": {
-    "good-morning": "Guten Morgen",
-    "good-afternoon": "Guten Tag",
-    "good-evening": "Guten Abend",
-    "hello": "Hallo"
-  },
-  "en": {
-    "good-morning": "Good Morning",
-    "good-afternoon": "Good Afternoon",
-    "good-evening": "Good Evening",
-    "hello": "Hello"
-  },
-  "fr": {
-    "good-morning": "Bonjour",
-    "good-afternoon": "Bon après-midi",
-    "good-evening": "Bonsoir",
-    "hello": "Salut"
-  },
-  "it": {
-    "good-morning": "Buongiorno",
-    "good-afternoon": "Buon pomeriggio",
-    "good-evening": "Buona sera",
-    "hello": "Ciao"
-  }
-}
-</i18n>

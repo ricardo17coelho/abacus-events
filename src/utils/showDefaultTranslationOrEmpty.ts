10fc257fc2823c8a33ef5a3ec237e7c8a99b3828
Ricardo Coelho <ricardo.coelho@abacus.ch>
2025-05-07 08:53:30 +0200

    fix(i18n): fn showDefaultTranslationOrEmpty

--- src/utils/showDefaultTranslationOrEmpty.ts
+++ src/utils/showDefaultTranslationOrEmpty.ts
@@ -2,8 +2,9 @@ import i18n from '@/plugins/i18n';
 
 export function showDefaultTranslationOrEmpty(value?: Record<string, string>) {
   if (!value) return '';
-  return Object.keys(value).includes(i18n.global.locale.value)
-    ? value[i18n.global.locale.value]
+  const locale = i18n.global.locale;
+  return Object.keys(value).includes(locale)
+    ? value[locale]
     : Object.values(value).length > 0
       ? Object.values(value)[0]
       : '';

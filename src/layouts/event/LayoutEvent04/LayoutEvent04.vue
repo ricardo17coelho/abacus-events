ee81adcba6a66db87594a36209fa45cc97b59d39
Ricardo Coelho <ricardo.coelho@abacus.ch>
2025-07-14 08:50:18 +0200

    WIP

--- src/layouts/event/LayoutEvent04/LayoutEvent04.vue
+++ src/layouts/event/LayoutEvent04/LayoutEvent04.vue
@@ -1,4 +1,5 @@
 <template>
+  <LayoutEvent02AppBar v-if="!smAndDown" />
   <v-main scrollable>
     <LayoutEvent04Mobile v-if="smAndDown" />
     <LayoutEvent04Desktop v-else />
@@ -10,6 +11,7 @@ import { useDisplay } from 'vuetify';
 
 import LayoutEvent04Mobile from '@/layouts/event/LayoutEvent04/LayoutEvent04Mobile.vue';
 import LayoutEvent04Desktop from '@/layouts/event/LayoutEvent04/LayoutEvent04Desktop.vue';
+import LayoutEvent02AppBar from '@/layouts/event/LayoutEvent02/LayoutEvent02AppBar.vue';
 
 const { smAndDown } = useDisplay();
 </script>

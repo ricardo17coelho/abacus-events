42698a6b006a3555f3fe1a0b27bc81d30c091963
Ricardo Coelho <ricardo.coelho@abacus.ch>
2025-07-18 13:03:22 +0200

    WIP

--- src/components/SWReloadPrompt.vue
+++ src/components/SWReloadPrompt.vue
@@ -32,7 +32,7 @@ import { useI18n } from 'vue-i18n';
 
 const { t } = useI18n();
 
-const showUpdatePrompt = ref(true);
+const showUpdatePrompt = ref(false);
 
 const updateSW = registerSW({
   onNeedRefresh() {

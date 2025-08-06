67d6784476f71b2cd418510f1f5d1a7103f4feeb
Ricardo Coelho <ricardo.coelho@abacus.ch>
2025-08-06 12:50:29 +0200

    feat(EventInformations): use only 'panels' view

--- src/layouts/event/LayoutEvent04/shared/LayoutEvent04SharedTabInformations.vue
+++ src/layouts/event/LayoutEvent04/shared/LayoutEvent04SharedTabInformations.vue
@@ -3,26 +3,7 @@
     v-if="currentEvent && hasEventFeature(currentEvent, 'INFORMATIONS')"
     :id="`feature-${getEventFeatureIfExists(currentEvent, 'INFORMATIONS')?.id}`"
   >
-    <div class="d-flex align-center justify-end py-2">
-      <v-btn-toggle
-        v-model="currentView"
-        base-color="surface-light"
-        class="bg-surface-light pa-1"
-        color="black"
-        density="compact"
-      >
-        <v-btn
-          class="text-none flex-1-1-0"
-          icon="mdi-list-box"
-          value="panels"
-        />
-
-        <v-btn class="text-none flex-1-1-0" icon="mdi-list-box" value="chips" />
-      </v-btn-toggle>
-    </div>
-
-    <LayoutEvent04TabInformationsChips v-if="currentView === 'chips'" />
-    <LayoutEvent04TabInformationsPanels v-else-if="currentView === 'panels'" />
+    <LayoutEvent04TabInformationsPanels />
   </div>
 </template>
 
@@ -34,8 +15,6 @@ import {
 import { requireInjection } from '@/utils/injection.ts';
 import { CURRENT_EVENT_KEY } from '@/types/injectionKeys.ts';
 import LayoutEvent04TabInformationsPanels from './LayoutEvent04TabInformationsPanels.vue';
-import LayoutEvent04TabInformationsChips from './LayoutEvent04TabInformationsChips.vue';
 
 const currentEvent = requireInjection(CURRENT_EVENT_KEY);
-const currentView = ref<'chips' | 'panels'>('chips');
 </script>

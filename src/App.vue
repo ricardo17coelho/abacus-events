ca6862475a8f9e66a08478e164b62d8d59da1192
Ricardo Coelho <ricardo.coelho@abacus.ch>
2025-08-13 17:58:42 +0200

    build: remove sw and be sure that current installations will be deinstallled

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,6 @@
   <v-app>
     <router-view />
     <Toaster position="top-right" rich-colors />
-    <SWReloadPrompt />
   </v-app>
 </template>
 <script lang="ts" setup>
@@ -10,7 +9,6 @@ import 'vue-sonner/style.css';
 import { Toaster } from 'vue-sonner';
 import { useGoogleAnalytics } from '@/plugins/google-analytics';
 import { isProdEnv } from './utils';
-import SWReloadPrompt from '@/components/SWReloadPrompt.vue';
 
 if (isProdEnv()) {
   console.log('init google analytics');

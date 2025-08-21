512dbbf9e3beb358a0e5855a5b6db34dd5b6d1c9
Ricardo Coelho <ricardo.coelho@abacus.ch>
2025-08-21 20:08:05 +0200

    WIP

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -2,15 +2,21 @@
 import type { App } from 'vue';
 // Plugins & Libraries
 import pinia from '@/stores';
-import router from './router';
 import vuetify from './vuetify';
 import i18n from './i18n';
-import umami from './umami';
+import { isProdEnv } from '@/utils';
+import { VueUmamiPlugin } from '@jaseeey/vue-umami-plugin';
+import router from './router';
 
 export function registerPlugins(app: App) {
   app.use(pinia);
   app.use(vuetify);
   app.use(i18n);
-  app.use(umami);
+  if (isProdEnv()) {
+    app.use(VueUmamiPlugin, {
+      websiteID: import.meta.env.VITE_UMAMI_WEBSITE_ID,
+      scriptSrc: 'https://cloud.umami.is/script.js',
+    });
+  }
   app.use(router);
 }
